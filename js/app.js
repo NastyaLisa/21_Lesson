665a94937f73412b60659b80b2aa365432835412
NastyaLisa <lis.nastiona@gmail.com>
2023-01-13 14:22:45 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,59 +1,61 @@
 //Task 1
-// const car = {
-//   model: "Tesla",
-//   year: 2022,
-//   color: "pink",
-//   signal() {
-//     alert("fa!, fa!");
-//   },
-// };
+const message = "Task 1";
+alert(message);
 
-// car.color = "red";
-// car.type = "electric";
+const car = {
+  model: "Tesla",
+  year: 2022,
+  color: "pink",
+  signal() {
+    alert("fa!, fa!");
+  },
+};
 
-// console.log(car);
-// car.signal();
+car.color = "red";
+car.type = "electric";
 
-//Task 2 dont work
-// const salaries = {
-//   fronted: 12000,
-//   backend: 10000,
-//   designer: 8000,
-
-// total() {
-
-// let totalSalaries = 0;
-// const keys = Object.keys(salaries);
-
-// for (const key of keys) {
-//   totalSalaries += salaries[key];
-//     };
-//   }
-// };
-// salaries.total();
-
-// salaries.manager = 5000;
+console.log(car);
+car.signal();
 
 
+//Task 2
+const salaries = {
+  fronted: 12000,
+  backend: 10000,
+  designer: 8000,
+};
 
+function total(object = {}) {
+  let totalSalaries = 0;
+  const values = Object.values(salaries);
 
-// salaries.total();
+  for (const value of values) {
+    totalSalaries += value;
+  }
+  return totalSalaries;
+}
+console.log(total());
 
+salaries.manager = 5000;
+delete salaries.designer;
+
+console.table(salaries);
+console.log(total());
 
 // task 3
+
 function Computer(brand, system, cost) {
   this.brand = brand;
   this.system = system;
   this.cost = cost;
 }
 
-let dell = new Computer('Dell', 'windows', '800');
-let apple = new Computer('Apple', 'MAC OS', '1700');
+let dell = new Computer("Dell", "windows", "800");
+let apple = new Computer("Apple", "MAC OS", "1700");
 
 console.log(dell.system);
 console.log(apple.cost);
-console.log(apple)
-
+console.log(apple);
 
 // task 4
 function Computer(brand, system, cost) {
@@ -61,22 +63,20 @@ function Computer(brand, system, cost) {
   this.system = system;
   this.cost = cost;
   this[Symbol.toPrimitive] = function (hint) {
-  
-  switch (hint) {
-    case 'string':
-      return this.brand
-    case 'number':
-      return this.cost
-    case 'default':
-      return this.brand + ' ' + this.system + ' ' + this.cost
-  }
+    switch (hint) {
+      case "string":
+        return this.brand;
+      case "number":
+        return this.cost;
+      case "default":
+        return this.brand + " " + this.system + " " + this.cost;
+    }
+  };
 }
-}
-let dell2 = new Computer('Dell', 'windows', 800);
-let apple2 = new Computer('Apple', 'MAC OS', 1700);
-let groupComputer = dell + ' ' + '//' + ' ' + apple;
+let dell2 = new Computer("Dell", "windows", 800);
+let apple2 = new Computer("Apple", "MAC OS", 1700);
+let groupComputer = dell + " " + "//" + " " + apple;
 
-
-console.log(groupComputer); 
+console.log(groupComputer);
 console.log(String(dell2));
 console.log(+apple2);
